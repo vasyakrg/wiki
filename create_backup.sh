b59adb239650c86f1a2f57038481c03e8f876100
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-05-21 16:29:26 +0700

    fix backup

--- create_backup.sh
+++ create_backup.sh
@@ -2,5 +2,5 @@
 
 source backup.env
 
-docker run --rm --env-file /srv/cfg/rocker-chat/backup.env --network slack_net --name slack_backup vasyakrg/backup:v2
+docker run --rm --env-file .env --network wiki --name wiki_backup vasyakrg/backup:v2
 echo "Backup create and save to ${STORAGE_HOST}/${STORAGE_PATH}"
