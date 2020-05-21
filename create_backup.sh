9df5385a6fcf23a5b42b19fb097bf1a266d53c40
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-05-21 16:32:40 +0700

    fix env

--- create_backup.sh
+++ create_backup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-source backup.env
-
 docker run --rm --env-file .env --network wiki --name wiki_backup vasyakrg/backup:v2
 echo "Backup create and save to ${STORAGE_HOST}/${STORAGE_PATH}"
