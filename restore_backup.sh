fc66d2e2a103640408754aa381c6076e56c9022b
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-06-13 20:14:47 +0700

    add restore script

---
+++ restore_backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source .env
+
+FILE=$1
+
+[[ ! -z $FILE ]] && {
+    echo "Usage: ./restore_backup.sh <dumpp file>"
+    exit 1
+}
+
+cat $FILE | docker exec -i ${CONTAINER_NAME_DB} psql -Upostgres
