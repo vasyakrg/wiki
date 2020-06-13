f55ba02f5b4bd4c7e5e5f67ac222ff6d2f5904e7
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-06-13 21:56:35 +0700

    fix restore

--- restore_backup.sh
+++ restore_backup.sh
@@ -9,4 +9,11 @@ FILE=$1
     exit 1
 }
 
+docker-compose down
+docker volume rm ${SERVICE_NAME}_${SERVICE_NAME}_${CONTAINER_NAME_DB}
+
+docker-compose -f docker-compose-db.yml up -d
+
 docker exec -i "${SERVICE_NAME}_${CONTAINER_NAME_DB}" pg_restore -C --clean --no-acl --no-owner -U "${DBUSER}" -d "${DBTABLE}" < ${FILE}
+
+docker-compose up -d
